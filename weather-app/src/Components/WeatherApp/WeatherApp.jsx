4886ad646630602b43a5441f19f6de07aa99c441
Serega <sverdlov.urchin2000@yandex.ru>
2023-10-21 21:28:24 +0300

    add elements with wind-speed and humidity-percent

--- weather-app/src/Components/WeatherApp/WeatherApp.jsx
+++ weather-app/src/Components/WeatherApp/WeatherApp.jsx
@@ -16,7 +16,28 @@ const WeatherApp = () => {
             <div className="top-bar">
                 <input type="text" className="cityInput" placeholder="Search"/>
                 <div className="search-icon">
-                    <img src={search_icon} alt="" />
+                    <img src={search_icon} alt=""/>
+                </div>
+            </div>
+            <div className="weather-image">
+                <img src={cloud_icon} alt=""/>
+            </div>
+            <div className="weather-temp">24</div>
+            <div className="weather-location">London</div>
+            <div className="data-container">
+                <div className="element">
+                    <img src={humidity_icon} alt="" className="icon"/>
+                    <div className="data">
+                        <div className="humidity-percent">64%</div>
+                        <div className="text">Wind Speed</div>
+                    </div>
+                </div>
+                <div className="element">
+                    <img src={wind_icon} alt="" className="icon"/>
+                    <div className="data">
+                        <div className="wind-speed">18 km/h</div>
+                        <div className="text">Wind Speed</div>
+                    </div>
                 </div>
             </div>
         </div>

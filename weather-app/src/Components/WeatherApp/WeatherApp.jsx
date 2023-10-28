0c94bd0e73133c547e72fe5cb81b38e3e973ecb9
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2023-10-28 23:41:32 +0300

    add change image with Weather

--- weather-app/src/Components/WeatherApp/WeatherApp.jsx
+++ weather-app/src/Components/WeatherApp/WeatherApp.jsx
@@ -11,7 +11,7 @@ import snow_icon from "../../Assets/snow.png"
 import rain_icon from "../../Assets/rain.png"
 
 const WeatherApp = () => {
-
+    const [wicon, setWicon] = React.useState(cloud_icon)
     const api_key = "4b2a0fb490c3400ba1278338a743366e"
     const search = async () => {
         const element = document.getElementsByClassName("cityInput")
@@ -26,11 +26,58 @@ const WeatherApp = () => {
         const temperature = document.getElementsByClassName("weather-temp")
         const location = document.getElementsByClassName("weather-location")
 
-        humidity[0].innerHTML = data.main.humidity;
-        console.log(data)
-        wind[0].innerHTML = data.wind.speed;
-        temperature[0].innerHTML = data.main.temp;
+        humidity[0].innerHTML = data.main.humidity + " %";
+        wind[0].innerHTML = Math.floor(data.wind.speed) + " km/h";
+        temperature[0].innerHTML = Math.floor(data.main.temp) + " °c";
         location[0].innerHTML = data.name;
+
+        switch (data.weather[0].icon) {
+            case "01d":
+                setWicon(clear_icon)
+                break;
+            case "01n":
+                setWicon(clear_icon)
+                break;
+            case "02d":
+                setWicon(cloud_icon)
+                break
+            case "02n":
+                setWicon(cloud_icon)
+                break;
+            case "03d":
+                setWicon(drizzle_icon)
+                break;
+            case "03n":
+                setWicon(drizzle_icon)
+                break;
+            case "04d":
+                setWicon(drizzle_icon)
+                break;
+            case "04n":
+                setWicon(drizzle_icon)
+                break;
+            case "09d":
+                setWicon(rain_icon)
+                break;
+            case "09n":
+                setWicon(rain_icon)
+                break;
+            case "10d":
+                setWicon(rain_icon)
+                break;
+            case "10n":
+                setWicon(rain_icon)
+                break
+            case "13d":
+                setWicon(snow_icon)
+                break;
+            case "13n":
+                setWicon(snow_icon)
+                break
+            default:
+                setWicon(clear_icon);
+        }
+
     }
     return (
         <div className='container'>
@@ -43,7 +90,7 @@ const WeatherApp = () => {
                 </div>
             </div>
             <div className="weather-image">
-                <img src={cloud_icon} alt=""/>
+                <img src={wicon} alt=""/>
             </div>
             <div className="weather-temp">24</div>
             <div className="weather-location">London</div>
